4b36e0144006eea80f156abb914485a3f043a700
SlyPuffin <dan.c.voss@gmail.com>
2023-03-11 15:12:09 -0800

    Added evaluation for checkboxes. If button is pressed, each check box is toggle programmatically.

--- src/tui.rs
+++ src/tui.rs
@@ -1,29 +1,38 @@
 use cursive::Cursive;
-use cursive::views::{Checkbox, LinearLayout};
+use cursive::traits::Nameable;
+use cursive::views::Checkbox;
+use cursive::views::{LinearLayout, Button};
 
 fn main() {
     let mut siv = cursive::default();
 
-    // Create a linear layout with horizontal orientation
-    let mut grid = LinearLayout::horizontal();
+    let mut grid = LinearLayout::vertical();
 
-    // Add 3 vertical linear layouts to the horizontal layout
-    for _ in 0..3 {
-        let mut row = LinearLayout::vertical();
+    for y in 0..16 {
+        let mut row = LinearLayout::horizontal();
 
-        // Add 16 checkboxes to each vertical layout
-        for _ in 0..16 {
-            row.add_child(Checkbox::new());
+        for x in 0..3 {
+            row.add_child(Checkbox::new()
+                .with_name(format!("{}{}", x, y).as_str()));
         }
 
-        // Add the vertical layout to the horizontal layout
         grid.add_child(row);
     }
 
-    // Add the grid layout to the TUI
+    let mut button = Button::new("Evaluate", evaluate);
+    grid.add_child(button);
+
     siv.add_layer(grid);
 
-    // Run the TUI event loop
     siv.run();
 }
 
+fn evaluate(s: &mut Cursive) {
+    for y in 0..16 {
+        for x in 0..3 {
+            s.call_on_name(format!("{}{}", x, y).as_str(), |view: &mut Checkbox| {
+                view.set_checked(!view.is_checked());
+            });
+        }
+    }
+}
