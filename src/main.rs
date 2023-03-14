7a598d56ef331603cf105e35345536927a6415b7
SlyPuffin <dan.c.voss@gmail.com>
2023-03-14 14:38:14 -0700

    Transitioned the CSV logic to the TUI. Runs audio as before based on TUI prompts

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,68 @@
-use std::io::BufReader;
+use cursive::traits::Nameable;
+use cursive::views::Checkbox;
+use cursive::views::{Button, LinearLayout};
+use cursive::Cursive;
 use rodio::{OutputStream, PlayError, Source};
+use std::io::BufReader;
 use std::time::Duration;
-use csv::Reader;
-use std::fs::File;
 
 fn main() {
-    let delay_time = 300;
+    let mut siv = cursive::default();
 
-    let csv_file = File::open("drums_2.csv").unwrap();
-    let reader = BufReader::new(csv_file);
-    let mut csv_reader = Reader::from_reader(reader);
+    let mut grid = LinearLayout::vertical();
 
+    for y in 0..16 {
+        let mut row = LinearLayout::horizontal();
+
+        for x in 0..3 {
+            row.add_child(Checkbox::new().with_name(format!("{}{}", x, y).as_str()));
+        }
+
+        grid.add_child(row);
+    }
+
+    let invert_button = Button::new("Invert", invert);
+    grid.add_child(invert_button);
+    let play_button = Button::new("Play", play);
+    grid.add_child(play_button);
+
+    siv.add_layer(grid);
+
+    siv.run();
+}
+
+fn invert(s: &mut Cursive) {
+    for y in 0..16 {
+        for x in 0..3 {
+            s.call_on_name(format!("{}{}", x, y).as_str(), |view: &mut Checkbox| {
+                view.set_checked(!view.is_checked());
+            });
+        }
+    }
+}
+
+fn play(s: &mut Cursive) {
     let mut cymbal_input_vec: Vec<bool> = Vec::new();
     let mut kick_input_vec: Vec<bool> = Vec::new();
     let mut snare_input_vec: Vec<bool> = Vec::new();
     let mut cymbal_output_vec: Vec<bool> = Vec::new();
     let mut kick_output_vec: Vec<bool> = Vec::new();
     let mut snare_output_vec: Vec<bool> = Vec::new();
+    let delay_time = 300;
 
-    for result in csv_reader.records() {
-        let record = result.unwrap();
-        let mut iterator = 0;
-        for field in record.iter() {
-            if iterator == 0 {
-                if field == String::from("x") {
-                    cymbal_input_vec.push(true);
-                } else {
-                    cymbal_input_vec.push(false);
+    for y in 0..16 {
+        for x in 0..3 {
+            s.call_on_name(format!("{}{}", x, y).as_str(), |view: &mut Checkbox| {
+                if x == 0 {
+                    cymbal_input_vec.push(view.is_checked());
                 }
-            }
-            if iterator == 1 {
-                if field == String::from("x") {
-                    kick_input_vec.push(true);
-                } else {
-                    kick_input_vec.push(false);
+                if x == 1 {
+                    kick_input_vec.push(view.is_checked());
                 }
-            }
-            if iterator == 2 {
-                if field == String::from("x") {
-                    snare_input_vec.push(true);
-                } else {
-                    snare_input_vec.push(false);
+                if x == 2 {
+                    snare_input_vec.push(view.is_checked());
                 }
-            }
-            iterator = iterator + 1;
+            });
         }
     }
 
@@ -72,25 +91,48 @@ fn main() {
     for i in 0..cymbal_input_vec.len() {
         let delay = delay_time * i;
         if cymbal_input_vec.remove(0) {
-            cymbal_vec.push(stream_handle.play_raw(cymbal_source.clone().delay(Duration::from_millis(delay as u64)).convert_samples()));
+            cymbal_vec.push(
+                stream_handle.play_raw(
+                    cymbal_source
+                        .clone()
+                        .delay(Duration::from_millis(delay as u64))
+                        .convert_samples(),
+                ),
+            );
             cymbal_output_vec.push(true);
         } else {
             cymbal_output_vec.push(false);
         }
         if kick_input_vec.remove(0) {
-            kick_vec.push(stream_handle.play_raw(kick_source.clone().delay(Duration::from_millis(delay as u64)).convert_samples()));
+            kick_vec.push(
+                stream_handle.play_raw(
+                    kick_source
+                        .clone()
+                        .delay(Duration::from_millis(delay as u64))
+                        .convert_samples(),
+                ),
+            );
             kick_output_vec.push(true);
         } else {
             kick_output_vec.push(false);
         }
         if snare_input_vec.remove(0) {
-            snare_vec.push(stream_handle.play_raw(snare_source.clone().delay(Duration::from_millis(delay as u64)).convert_samples()));
+            snare_vec.push(
+                stream_handle.play_raw(
+                    snare_source
+                        .clone()
+                        .delay(Duration::from_millis(delay as u64))
+                        .convert_samples(),
+                ),
+            );
             snare_output_vec.push(true);
         } else {
             snare_output_vec.push(false);
         }
     }
-    std::thread::sleep(Duration::from_millis((cymbal_output_vec.len() * delay_time) as u64));
+    std::thread::sleep(Duration::from_millis(
+        (cymbal_output_vec.len() * delay_time) as u64,
+    ));
     for _i in 0..cymbal_output_vec.len() {
         if cymbal_output_vec.remove(0) {
             drop(cymbal_vec.pop());
